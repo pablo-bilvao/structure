db0d293c7866d7855e496619f8d242edf2ec88a1
= <pablo.bilvao@123seguro.com.ar>
2019-03-25 17:59:05 -0300

    model update

--- src/Views/structure/model.blade.php
+++ src/Views/structure/model.blade.php
@@ -5,5 +5,6 @@
 
 	class {{ ucfirst($service) }} extends Model
 	{
+	    @if(in_array("id", $fillables))public $incrementing = false;@endif
 	    protected $fillable = [@foreach($fillables as $key => $fillable)'{{$fillable}}'@if( count($fillables) -1 != $key),@endif @endforeach];
 	}
